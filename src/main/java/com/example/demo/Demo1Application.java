13e4ca9711f7c1f049cb3304af35ecdaa083a254
29010-55-11 <290-56-11@exchange.ad.afpanet>
2024-07-09 10:56:24 +0200

    test2

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CalculatorApplication {
+public class Demo1Application {
     public static void main(String[] args) {
-        SpringApplication.run(CalculatorApplication.class, args);
+        SpringApplication.run(Demo1Application.class, args);
     }
 }
 
